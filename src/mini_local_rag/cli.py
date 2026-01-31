9207b05a7c443da5eb790bee5203f7d24eaa1f4b
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-01-31 14:40:07 +0200

    remove things from main

--- src/mini_local_rag/cli.py
+++ src/mini_local_rag/cli.py
@@ -1,13 +1,8 @@
 
-from mini_local_rag.config import Config
-from mini_local_rag.pipeline_builder import PipelineBuilder
 
 
 def main() -> None:
-    builder = PipelineBuilder(config=Config(enable_local_models=True,show_logs=True))
-    # pipeline = builder.get_ingestion_pipeline(file_path=r"""C:\Users\STAVRE02\Documents\dev\mini-local-rag\documents\E3 Structure - Document 2.pdf""")
-    pipeline = builder.get_ask_pipeline(question="what is the ICH E3 guidance ")
-    pipeline.execute()
+    pass
 
 
 if __name__ == "__main__":
