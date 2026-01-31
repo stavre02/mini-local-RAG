89b680639871f7288d7e816a6e85db628215ddab
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-01-31 19:59:54 +0200

    update cli to not create objects for each request and have context

--- src/mini_local_rag/cli.py
+++ src/mini_local_rag/cli.py
@@ -1,89 +1,114 @@
 import argparse
 import shlex
+import sys
+from typing import Optional
 
 from mini_local_rag.config import Config
 from mini_local_rag.pipeline_builder import PipelineBuilder
 
-def documents_cmd(args: argparse.Namespace) -> None:
-    PipelineBuilder(config=Config(show_logs=args.show_logs)).get_documents().execute()
 
-def ingest_cmd(args: argparse.Namespace) -> None:
-    PipelineBuilder(config=Config(show_logs=args.show_logs)).get_ingestion_pipeline(file_path=args.file_path).execute()
+class AppContext:
+    parser:argparse.ArgumentParser
+    interactive:bool = False
+    config: Optional[Config]
+    parser: Optional[argparse.ArgumentParser]
+    builder: Optional[PipelineBuilder]
 
-def ask_cmd(args: argparse.Namespace) -> None:
-    PipelineBuilder(config=Config(show_logs=args.show_logs)).get_ask_pipeline(question=args.question).execute()
 
-def interactive_mode(parser:argparse.ArgumentParser)->None:
-    print("Entering interactive mode. Type 'exit' to quit.")
-    
-    while True:
-        try:
-            user_input = input(">>> ")
+    def __init__(self):
+        if '-i' in sys.argv or '--interactive' in sys.argv: self.interactive=True
 
-            if user_input.lower() == 'exit':
-                print("Exiting interactive mode.")
-                break
 
+
+    def documents_cmd(self,args: argparse.Namespace) -> None:
+        self.get_builder().get_documents().execute()
+
+    def ingest_cmd(self,args: argparse.Namespace) -> None:
+        self.get_builder().get_ingestion_pipeline(file_path=args.file_path).execute()
+
+    def ask_cmd(self,args: argparse.Namespace) -> None:
+        self.get_builder().get_ask_pipeline(question=args.question).execute()
+
+    def interactive_mode(self)->None:
+        print("Entering interactive mode. Type 'exit' to quit.")
+        
+        while True:
+            try:
+                user_input = input(">>> ")
+
+                if user_input.lower() == 'exit':
+                    print("Exiting interactive mode.")
+                    break
+
+                    
+                args_list = shlex.split(user_input)
+                args = self.parser.parse_args(args=args_list)
                 
-            args_list = shlex.split(user_input)
-            args = parser.parse_args(args=args_list)
-            
+                if not args.command:
+                    self.parser.print_help()
+                    return
+
+                args.func(args)
+            except SystemExit as e:
+                # make args parse not exit te app 
+                pass
+            except Exception as e:
+                pass
+
+
+    def get_builder(self) -> PipelineBuilder:
+        if not hasattr(self,"builder"):
+            self.builder = PipelineBuilder(config=self.config)
+            return self.builder
+        return self.builder
+
+    def build_parser(self) -> None:
+
+        prog = "" if self.interactive else None
+        parser = argparse.ArgumentParser(
+                description="Pipeline command line interface for processing PDF reports and as questions.",
+                prog=prog
+            )
+        subparsers = parser.add_subparsers(dest="command")
+        
+        # documents command
+        documents = subparsers.add_parser("documents", help="List all documents by path")
+        documents.set_defaults(func=self.documents_cmd)
+
+        # ingest command
+        ingest = subparsers.add_parser("ingest", help="Ingest a document")
+        ingest.add_argument("file_path", help="Path to the pdf file to ingest")
+        ingest.set_defaults(func=self.ingest_cmd)
+
+        # ask command
+        ask = subparsers.add_parser("ask", help="Ask a question")
+        ask.add_argument("question", help="Question to ask")
+        ask.set_defaults(func=self.ask_cmd)
+        parser.add_argument("--show-logs", action="store_true", help="Display debug logs, can be used only on start up: example '-i --show-logs', '--show-logs documents")
+        parser.add_argument("--interactive","-i", action="store_true", help="Interactive mode, Can be used only on start up")
+        self.parser = parser
+
+
+    def run(self) -> None:
+        self.build_parser()
+        args = self.parser.parse_args()
+
+        self.config = Config(show_logs=args.show_logs)
+        if (args.interactive):
+            self.interactive_mode()
+        else:    
             if not args.command:
-                parser.print_help()
+                self.parser.print_help()
                 return
 
             args.func(args)
-        except SystemExit as e:
-            # make args parse not exit te app 
-            pass
-        except Exception as e:
-            pass
 
 
-def build_parser(interactive:bool) -> argparse.ArgumentParser:
-    prog = "" if interactive else None
-    parser = argparse.ArgumentParser(
-            description="Pipeline command line interface for processing PDF reports and as questions.",
-            prog=prog
-        )
-    subparsers = parser.add_subparsers(dest="command")
 
-    # documents command
-    documents = subparsers.add_parser("documents", help="List all documents by path")
-    documents.add_argument("--show-logs", action="store_true", help="Display debug logs")
-    documents.set_defaults(func=documents_cmd)
-
-    # ingest command
-    ingest = subparsers.add_parser("ingest", help="Ingest a document")
-    ingest.add_argument("file_path", help="Path to the pdf file to ingest")
-    ingest.add_argument("--show-logs", action="store_true", help="Display debug logs")
-    ingest.set_defaults(func=ingest_cmd)
-
-    # ask command
-    ask = subparsers.add_parser("ask", help="Ask a question")
-    ask.add_argument("question", help="Question to ask")
-    ask.add_argument("--show-logs", action="store_true", help="Display debug logs")
-    ask.set_defaults(func=ask_cmd)
-
-    return parser
-
-def main():
-    import sys
+def main():    
+    AppContext().run()
     
     
-    if '-i' in sys.argv or '--interactive' in sys.argv:
-        # If -i is passed, run the interactive mode
-        parser = build_parser(interactive=True)
-        interactive_mode(parser)
-    else:
-        parser = build_parser(interactive=False)
-        args = parser.parse_args()
-    
-        if not args.command:
-            parser.print_help()
-            return
-
-        args.func(args)
 
 
 if __name__ == "__main__":
