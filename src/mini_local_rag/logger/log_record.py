7a76676a63ddb358c5f31058d5bd1509de39c1c5
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-01-31 12:30:07 +0200

    create structured logger

---
+++ src/mini_local_rag/logger/log_record.py
@@ -0,0 +1,31 @@
+import traceback
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class LogRecord(BaseModel):
+    trace_id: str
+    plan: List[str]
+    latency: Dict[str, float]
+    errors: List[Dict[str, Any]]
+    # extra fields..
+    # question: Optional[str]
+    # retrieval: Optional[List[Dict[str, str]]]
+    # draft_tokens: int
+    @staticmethod
+    def create(trace_id:str,plan: List[str],latency: Optional[Dict[str, float]]={},**kwargs):
+        return LogRecord(trace_id=trace_id,plan=plan,latency=latency,errors=[],**kwargs)
+    
+    def add_error(self,e:Exception):
+        stack_trace_string = "".join(traceback.format_exception(type(e), e, e.__traceback__))
+        self.errors.append({
+            "exception":e.__class__.__name__,
+            "message":str(e),
+            "stacktrace":stack_trace_string
+        
+        })
+
+    # Pydantic configuration to allow extra fields
+    class Config:
+        extra = "allow"  # Allow extra fields not defined in the model
---
+++ src/mini_local_rag/logger/structured_logger.py
@@ -0,0 +1,53 @@
+
+import logging
+import os
+import sys
+from typing import Optional
+
+from mini_local_rag.config import Config
+from mini_local_rag.logger.log_record import LogRecord
+
+
+class JsonFormatter(logging.Formatter):
+    """Custom formatter that outputs log records as JSON."""
+
+    def __init__(self, fmt: Optional[str] = None, datefmt: Optional[str] = None,):
+        super().__init__(fmt, datefmt)
+
+    
+    def format(self, record):
+        data: LogRecord = getattr(record, 'data')
+        return data.model_dump_json(indent=3)
+
+class StructuredLogger:
+    __log_file:str ="logs" 
+    def __init__(self, config:Config):
+        self.logger = logging.getLogger("structured_logger")
+        self.logger.setLevel(logging.DEBUG)
+        
+        # JSON formatter
+        formatter = JsonFormatter()
+
+        # File handler (appends to file)
+        cwd = os.getcwd()
+        log_dir = os.path.join(cwd, config.show_logs)
+        log_file_path = os.path.join(log_dir, self.__log_file)
+        os.makedirs(log_dir, exist_ok=True)
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+        # Console handler (optional)
+        if config.show_logs:
+            console_handler = logging.StreamHandler(sys.stdout)
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
+
+    def log(self, record: LogRecord):
+        """Log a message with a specified level and structured fields."""
+        extra = {
+            'data' :record
+        }
+        
+        self.logger.debug(msg=f"log record for trace_id {record.trace_id}", extra=extra)
+
