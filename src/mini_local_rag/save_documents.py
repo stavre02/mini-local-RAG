f987acc7f45be8d3f0562a07276ae7aab8e9031a
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-01-31 16:20:09 +0200

    rm unused file

--- src/mini_local_rag/save_documents.py
+++
@@ -1,7 +0,0 @@
-
-class SaveDataStep(Step):
-    label = "Saving data"
-    _vector_store= VectorStore()
-    def execute(self, context: Dict[str, Any]) -> None:
-        documents: list[Document] = context["documents"]        
-        self._vector_store.saveAll(documents)
