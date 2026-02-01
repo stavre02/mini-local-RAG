4aeba9932fb674d844aa06708b21bf88f09af03d
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-02-01 11:32:44 +0200

    make interactive mode have the ability to show logs

--- src/mini_local_rag/cli.py
+++ src/mini_local_rag/cli.py
@@ -5,45 +5,52 @@ from typing import Optional
 
 from mini_local_rag.config import Config
 from mini_local_rag.pipeline_builder import PipelineBuilder
+from rich import print as rprint
 
 
 class AppContext:
+    """Handles the context for the application, including argument parsing and interactive mode."""
+
     parser:argparse.ArgumentParser
-    interactive:bool = False
     config: Optional[Config]
     parser: Optional[argparse.ArgumentParser]
     builder: Optional[PipelineBuilder]
 
 
-    def __init__(self):
-        if '-i' in sys.argv or '--interactive' in sys.argv: self.interactive=True
-
-
 
     def documents_cmd(self,args: argparse.Namespace) -> None:
+        """Handle the 'documents' command: list all documents."""
+
         self.get_builder().get_documents().execute()
 
     def ingest_cmd(self,args: argparse.Namespace) -> None:
+        """Handle the 'ingest' command: ingest a document from the given file path."""
+
         self.get_builder().get_ingestion_pipeline(file_path=args.file_path).execute()
 
     def ask_cmd(self,args: argparse.Namespace) -> None:
+        """Handle the 'ask' command: process the given question."""
+
         self.get_builder().get_ask_pipeline(question=args.question).execute()
 
     def interactive_mode(self)->None:
-        print("Entering interactive mode. Type 'exit' to quit.")
+        """Start an interactive mode for the user to input commands."""
+
+        rprint("Entering interactive mode. Type 'exit' to quit.")
         
         while True:
             try:
                 user_input = input(">>> ")
 
                 if user_input.lower() == 'exit':
-                    print("Exiting interactive mode.")
+                    rprint("Exiting interactive mode.")
                     break
 
                     
                 args_list = shlex.split(user_input)
                 args = self.parser.parse_args(args=args_list)
-                
+                self.config.show_logs=args.show_logs
+
                 if not args.command:
                     self.parser.print_help()
                     return
@@ -57,14 +64,18 @@ class AppContext:
 
 
     def get_builder(self) -> PipelineBuilder:
+        """Lazy load the PipelineBuilder, if it's not already initialized.Use only one instance """
+
         if not hasattr(self,"builder"):
             self.builder = PipelineBuilder(config=self.config)
             return self.builder
         return self.builder
 
     def build_parser(self) -> None:
+        """Build and configure the argument parser with subcommands."""
 
-        prog = "" if self.interactive else None
+        prog ="" if '-i' in sys.argv or '--interactive' in sys.argv else None
+            
         parser = argparse.ArgumentParser(
                 description="Pipeline command line interface for processing PDF reports and as questions.",
                 prog=prog
@@ -73,39 +84,46 @@ class AppContext:
         
         # documents command
         documents = subparsers.add_parser("documents", help="List all documents by path")
+        documents.add_argument("--show-logs", action="store_true", help="Display debug logs")
         documents.set_defaults(func=self.documents_cmd)
-
+        
         # ingest command
         ingest = subparsers.add_parser("ingest", help="Ingest a document")
         ingest.add_argument("file_path", help="Path to the pdf file to ingest")
+        ingest.add_argument("--show-logs", action="store_true", help="Display debug logs")
         ingest.set_defaults(func=self.ingest_cmd)
 
         # ask command
         ask = subparsers.add_parser("ask", help="Ask a question")
         ask.add_argument("question", help="Question to ask")
+        ask.add_argument("--show-logs", action="store_true", help="Display debug logs")
         ask.set_defaults(func=self.ask_cmd)
-        parser.add_argument("--show-logs", action="store_true", help="Display debug logs, can be used only on start up: example '-i --show-logs', '--show-logs documents")
+
         parser.add_argument("--interactive","-i", action="store_true", help="Interactive mode, Can be used only on start up")
         self.parser = parser
 
 
     def run(self) -> None:
+        """Run the application: build the parser, parse arguments, and execute commands."""
+
         self.build_parser()
         args = self.parser.parse_args()
 
-        self.config = Config(show_logs=args.show_logs)
+        self.config = Config(enable_local_models =True)
         if (args.interactive):
             self.interactive_mode()
         else:    
             if not args.command:
                 self.parser.print_help()
                 return
-
+            self.config.show_logs=args.show_logs
             args.func(args)
 
 
 
-def main():    
+def main():
+    """Main entry point of the application."""
+    
     AppContext().run()
     
     
--- src/mini_local_rag/logger/structured_logger.py
+++ src/mini_local_rag/logger/structured_logger.py
@@ -12,22 +12,49 @@ class JsonFormatter(logging.Formatter):
     """Custom formatter that outputs log records as JSON."""
 
     def __init__(self, fmt: Optional[str] = None, datefmt: Optional[str] = None,):
+        """
+        Initialize the JSON formatter.
+
+        Args:
+            fmt (Optional[str]): Format for the log message (not used for JSON output).
+            datefmt (Optional[str]): Format for the date and time (not used for JSON output).
+        """
         super().__init__(fmt, datefmt)
 
     
     def format(self, record):
+        """
+        Format the log record as JSON.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The log record formatted as a JSON string.
+        """
         data: LogRecord = getattr(record, 'data')
         return data.model_dump_json(indent=3)
 
 class StructuredLogger:
+    """Logger that outputs structured logs to both file and console, with JSON format."""
+
     __log_file:str ="logs" 
     def __init__(self, config:Config):
+        """
+        Initialize the StructuredLogger.
+
+        Args:
+            config (Config): Configuration object containing settings for logging.
+        """
         self.logger = logging.getLogger("structured_logger")
         self.logger.setLevel(logging.DEBUG)
+        self.config=config
+
         
         # JSON formatter
         formatter = JsonFormatter()
 
+        #log t file
         # File handler (appends to file)
         cwd = os.getcwd()
         log_dir = os.path.join(cwd, config.data_folder)
@@ -37,17 +64,27 @@ class StructuredLogger:
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
-        # Console handler (optional)
-        if config.show_logs:
-            console_handler = logging.StreamHandler(sys.stdout)
-            console_handler.setFormatter(formatter)
-            self.logger.addHandler(console_handler)
+
+        # log to console
+        self.console_logger = logging.getLogger("structured_logger_console")
+        self.console_logger.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        self.console_logger.addHandler(console_handler)
+
 
     def log(self, record: LogRecord):
-        """Log a message with a specified level and structured fields."""
+        """
+        Log a message with a specified level and structured fields.
+
+        Args:
+            record (LogRecord): The log record containing data to be logged.
+        """
         extra = {
             'data' :record
         }
-        
+        if (self.config.show_logs):
+            self.console_logger.debug(msg=f"log record for trace_id {record.trace_id}", extra=extra)
+
         self.logger.debug(msg=f"log record for trace_id {record.trace_id}", extra=extra)
 
