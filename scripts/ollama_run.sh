531f17a3941682d9ad849dfc27a06bf7d135a4ea
stavre02 <Efstathios.Stavridis@pfizer.com>
2026-02-01 16:07:41 +0200

    test update on ollama cause of the ssl issue

---
+++ scripts/ollama_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+nohup ollama serve &
+
+until ollama list >/dev/null 2>&1; do
+    echo "Waiting for ollama to start..."
+    sleep 2
+done
+
+ollama pull qwen3-embedding:4b 
+ollama pull llama3.2:1b 
+ollama pull gemma3:4b
